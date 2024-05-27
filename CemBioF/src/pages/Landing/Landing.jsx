7d2f3a2406e7b0859cf30cc43fa35fca3b56fb51
CamJMend <cam.jmend@gmail.com>
2024-05-27 16:47:48 -0600

    Front pt3

--- CemBioF/src/pages/Landing/Landing.jsx
+++ CemBioF/src/pages/Landing/Landing.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./Landing.css";
 import WaitlistForm from '../../components/WaitlistForm/WaitlistForm';
 import image_card1 from "../../assets/images/CardImage1.png";
 import image_card2 from "../../assets/images/CardImage2.png";
 import image_card3 from "../../assets/images/CardImage3.png";
-import video from "../../assets/videos/hookenz.mp4";
-
+import video from "../../assets/videos/SoftPrototype.mp4";
+import image_transition1 from "../../assets/images/Transition1.png";
+import image_transition2 from "../../assets/images/Transition2.png";
 
 const Landing = () => {
+    const transitionImageRef = useRef(null);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            if (window.scrollY > 760) {
+                transitionImageRef.current.src = image_transition2;
+            } else {
+                transitionImageRef.current.src = image_transition1;
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <>
             <div className='canvas'>
@@ -19,6 +38,21 @@ const Landing = () => {
             </div>
             <div className="solution">
                 <h2>Our Solution</h2>
+                <div className="product-description">
+                    <img ref={transitionImageRef} src={image_transition1} alt="Card Image 1" />
+                    <div className="product-description-texts">
+                        <h3>Description</h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna 
+                        aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                        <ul>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                            <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</li>
+                        </ul>
+                    </div>
+                </div>
                 <div className="cards">
                     <div className="card">
                         <img src={image_card1} alt="Card Image 1" />
@@ -47,7 +81,7 @@ const Landing = () => {
                 <WaitlistForm />
             </div>
         </>
-      )
+    );
 }
-    
-export default Landing
+
+export default Landing;

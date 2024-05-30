62a5595e72014a9dcb2a9c335bbf28e8b6b175d3
CamJMend <cam.jmend@gmail.com>
2024-05-29 18:53:57 -0600

    Deploy back

--- server/server.js
+++ server/server.js
@@ -12,6 +12,14 @@ const port = 5000;
 
 const dbURI = 'mongodb+srv://camjmend:X1Aczu92AoIvk9Ci@cembio.5muwcgy.mongodb.net/?retryWrites=true&w=majority&appName=CemBio';
 
+const corsOptions = {
+    origin: 'https://cembio-d18a9dly6-camila-jimenezs-projects.vercel.app', // Asegúrate de que esta URL sea exacta
+    optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+  
+
 mongoose.connect(dbURI, {
     useNewUrlParser: true
 });
